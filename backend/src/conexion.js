2fe89c71742eb58a4d5bc54b920beb860b4ecc38
Diego Sebastian Rojas Guerrero <diegorojas13579@gmail.com>
2024-11-22 11:24:11 -0600

    Segundo

--- backend/src/conexion.js
+++ backend/src/conexion.js
@@ -6,7 +6,7 @@ const DB_USER = process.env.DB_USER;
 const DB_PASSWORD = process.env.DB_PASSWORD;
 const DB_NAME = process.env.DB_NAME;
 const DB_CLUSTER = process.env.DB_CLUSTER;
-
+console.log(DB_USER)
 const conexion = `mongodb+srv://${DB_USER}:${DB_PASSWORD}@${DB_CLUSTER}/${DB_NAME}`;
 
 //const conexion = `mongodb+srv://basti13579:pXcipvZOIlRGJSaO@basti.hcpua.mongodb.net/TiendaVirtual`
